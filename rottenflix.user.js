406431d4ee2a343b4c5a33243b1ce94fdc4d34ba
Matt Blodgett <mattblodgett@gmail.com>
2010-02-20 20:38:02 -0500

    Updated to jQuery 1.4.2

--- rottenflix.user.js
+++ rottenflix.user.js
@@ -10,7 +10,7 @@
 // This technique for loading jQuery was necessary because @require would not work.
 
 var GM_jQuery = document.createElement('script');
-GM_jQuery.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js';
+GM_jQuery.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js';
 GM_jQuery.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(GM_jQuery);
 
